966966912ba94e18eaecf2e2641274ffbd42dda8
Kaio <115432893+staff-gj@users.noreply.github.com>
2024-01-11 17:37:09 -0300

    InformacoesAluno.js

--- src/components/pages/InformacoesAluno.js
+++ src/components/pages/InformacoesAluno.js
@@ -15,7 +15,7 @@ export default function InformacoesAluno(props){
         if (!aluno_id) return;
 
         function buscarInformacoesAluno() {
-            const url = 'https://cypriot-overcoat.000webhostapp.com/backend/receber_dados.php';
+            const url = 'https://cypriot-overcoat.000webhostapp.com/backend/aluno_especifico.php';
             const formData = new FormData();
             formData.append('aluno_id', aluno_id);
 
