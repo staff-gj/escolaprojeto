306bce9652a2e4310b4985c3963cb3817363e7f2
Kaio <115432893+staff-gj@users.noreply.github.com>
2024-01-10 22:25:30 -0300

    NovoAluno.js

--- src/components/pages/NovoAluno.js
+++ src/components/pages/NovoAluno.js
@@ -25,7 +25,7 @@ export default function NovoAluno(){
         e.preventDefault()
         console.log(informacoes)
 
-        const url = 'http://localhost/escoladb/cadastrar_aluno.php';
+        const url = 'http://backendteste.free.nf/cadastrar_aluno.php';
         let fData = new FormData();
   
         fData.append('periodo', periodo);
