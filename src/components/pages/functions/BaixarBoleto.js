2d44a65f98087c98796d2c1fa7fee8047989b4d5
Kaio <115432893+staff-gj@users.noreply.github.com>
2024-01-27 23:01:50 -0300

    Add files via upload

--- src/components/pages/functions/BaixarBoleto.js
+++ src/components/pages/functions/BaixarBoleto.js
@@ -4,7 +4,6 @@ import pdfFonts from 'pdfmake/build/vfs_fonts';
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 export function BaixarBoleto(nomeAluno, responsavel, valorCobrado) {
-    alert(`Bom dia ${nomeAluno}, com responsável ${responsavel}, gostaria de baixar seu boleto?`);
 
     const boletos = [];
 
@@ -14,12 +13,13 @@ export function BaixarBoleto(nomeAluno, responsavel, valorCobrado) {
 
         boletos.push(
             [
+                
                 { text: 'Carnê de Pagamento Mensal - Escolinha Carequinha V', bold: true, fontSize: 15 },
                 { text: 'Maria Neves Ferreira Alves - CNPJ 04177166000139', fontSize: 12 },
                 { text: 'Rua Anita Garibaldi – 224 - Florais Léa Luis Eduardo Magalhães - Bahia CEP 47850-000', fontSize: 12 },
                 { text: '\n' },
-                { text: `Nome do responsável: ${responsavel} `, fontSize: 12 },
-                { text: `Nome do aluno(a): ${nomeAluno} `, fontSize: 12 },
+                { text: `Nome do responsável: ${responsavel.toUpperCase()} `, fontSize: 12 },
+                { text: `Nome do aluno(a): ${nomeAluno.toUpperCase()} `, fontSize: 12 },
                 { text: `Vencimento: ${vencimentoFormatado}`, fontSize: 12 },
                 { text: `Valor Pago: R$`, fontSize: 12 },
                 { text: `Valor: R$${valorCobrado}`, fontSize: 12 },
