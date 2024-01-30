47362fff8ab6c1629b55a0ac2f176b76b00ace03
Kaio <115432893+staff-gj@users.noreply.github.com>
2024-01-29 21:00:24 -0300

    Alunos.js

--- src/components/pages/Alunos.js
+++ src/components/pages/Alunos.js
@@ -4,6 +4,8 @@ import styles from './Alunos.modules.css'
 import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faAddressCard } from '@fortawesome/free-solid-svg-icons';
 
  
 export default function Alunos(){
@@ -16,7 +18,7 @@ export default function Alunos(){
                 console.log(data)
             })
     }, [])
-     function apagarAluno(id){
+    function apagarAluno(id){
         const url = 'https://cypriot-overcoat.000webhostapp.com/backend/apagar_aluno.php';
         let fData = new FormData();
 
@@ -24,7 +26,6 @@ export default function Alunos(){
         console.log("Id do aluno: ",id)
 
         axios.post(url, fData).then(response=> console.log(response.data)).catch(error=> alert(error));  
-      window.location = '/alunos'
     }
     return(
         <div>
@@ -34,11 +35,11 @@ export default function Alunos(){
             {aluno.length > 0 ?(
                 aluno.map((alunos, index)=>(
                     <div className='informacoes_aluno' key={index}>
-                        <h1>{alunos.nome_aluno}</h1>
+                        <h1>{alunos.nome_aluno.toUpperCase()}</h1>
                         <p>{alunos.periodo}</p>
-                        <p>{alunos.serie_aluno}</p>
-                        <Link to={`/informacoes_aluno?id=${alunos.id}`} state={alunos}>Ver informações completas</Link>
-                        <button onClick={() => apagarAluno(alunos.id)}>Apagar Aluno</button>
+                        <p>{alunos.serie_aluno}</p><br />
+                        <Link className='botao_ver_informacoes' to={`/informacoes_aluno?id=${alunos.id}`} state={alunos}><FontAwesomeIcon icon={faAddressCard} />        Ver informações completas</Link>
+                        <button className='botao_apagar_aluno' onClick={() => apagarAluno(alunos.id)}><FontAwesomeIcon icon={faTrash} />         Apagar Aluno</button>
                     </div>
                 ))
             ) : (
